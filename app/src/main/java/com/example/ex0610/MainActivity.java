2fb226b7d5907729c1864911f735a2806c497a28
as6620 <as6620@bs.amalnet.k12.il>
2024-02-24 12:52:43 +0200

    second

--- app/src/main/java/com/example/ex0610/MainActivity.java
+++ app/src/main/java/com/example/ex0610/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.ex0610;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+    ConstraintLayout mainLayout;
+    Button btn;
+    Switch swDN;
+    ToggleButton tb;
+    int background1 = Color.parseColor("#FF5733");
+    int background2 = Color.parseColor("#33FF57");
+    int background3 = Color.parseColor("#5733FF");
+    int background4 = Color.parseColor("#33FFEC");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        tb = findViewById(R.id.tb);
+        swDN = findViewById(R.id.swDN);
+        mainLayout = findViewById(R.id.mainLayout);
+
     }
+
+
 }
